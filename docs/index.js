128eedc370b3153b8910203ff4a0cb8657a506da
Digitalknuckles Dev <digitalknucklesdev@gmail.com>
2025-07-12 23:45:09 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -80,7 +80,7 @@ function simulateTeleport(cidKey) {
           simulateTeleport("CID_DEFAULT_2");
 
           currentIndex = stateOrder.indexOf("CID_DEFAULT_2");
-        }, 1800);
+        }, 1500);
       };
     });
   } else {
